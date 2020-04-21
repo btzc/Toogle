498dd4030a87541904d611642bc289599200be9a
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2020-04-21 13:03:57 -0700

    cleaned up handlers for get/post

--- handler.js
+++ handler.js
@@ -1,26 +1,46 @@
+const sendResponse = (res, statusCode, headers, data) => {
+  res.writeHead(statusCode, headers);
+  res.end(data);
+}
+
+const parseData = (req, res) => {
+  let rawData = '';
+
+  req.on('data', (chunk) => { 
+    rawData += chunk; 
+  });
+
+  req.on('end', () => {
+    try {
+      console.log(JSON.parse(rawData));
+      const statusCode = 200;
+      const headers = {
+        'Content-Type': 'text/plain'
+      };
+      const data = 'Success';
+      sendResponse(res, statusCode, headers, data);
+    } catch (e) {
+      console.error(e.message);
+      const statusCode = 400;
+      const headers = {
+        'Content-Type': 'text/plain'
+      };
+      const data = 'Server error';
+      sentResponse(res, statusCode, headres, data);
+    }
+  });
+}
+
 module.exports = (req, res) => {
   if (req.method === 'GET') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    const statusCode = 200;
+    const headers = {
+      'Content-Type': 'text/plain'
+    };
+    const data = 'Hello, World!\n';
+
+    sendResponse(res, statusCode, headers, data); 
   } else {
-    let rawData = '';
-
-    req.on('data', (chunk) => { 
-      rawData += chunk; 
-    });
-
-    req.on('end', () => {
-      try {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Success');
-      } catch (e) {
-        console.error(e.message);
-        res.statusCode = 400;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Server error');
-      }
-    });
+    parseData(req, res);
   }
 };
