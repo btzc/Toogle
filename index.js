35964eacb787c9cbbd1242ba4c795d50e7fddcfd
Brandon Zhou-Charles <brandonzhoucharles@gmail.com>
2020-04-22 18:25:10 -0700

    added mongodb driver and setup initial connection

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const http = require('http');
 const handler = require('./handler');
+const MongoClient = require('mongodb').MongoClient;
+
+const dbName = 'toogle';
+const url = `mongodb://localhost:27017/${dbName}`;
+
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+client.connect(function(err) {
+  console.log(`Connected successfully to server`);
+  client.close();
+});
 
 const hostname = '127.0.0.1';
 const port = 8080;
